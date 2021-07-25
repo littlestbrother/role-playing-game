cdbec1b3c186f5cff67053799176f5a846885e25
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-07-25 15:12:45 -0400

    pass all tests creating classes for characters
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Aaron Kauffman <aaron.christian.kauffman@gmail.com>

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -1,16 +1,80 @@
 import { TestScheduler } from 'jest';
-import { Wizard } from './../src/rpg.js';
+import { Wizard, Player, Samurai, VinDiesel, Ninja, Warrior } from './../src/rpg.js';
 
 describe('Wizard', () => {
     test('should create Wizard class', () => {
-        let character = new Wizard('Marni');
-        expect(character.name).toEqual('Marni');
+        let character = new Wizard('Harry Potter');
+        expect(character.name).toEqual('Harry Potter');
         expect(character.intelligence).toEqual(10);
         expect(character.strength).toEqual(2);
         expect(character.magic).toEqual(10);
         expect(character.level).toEqual(1);
+        expect(character.hp).toEqual(90);
+
+    });
+  });
+  describe('Samurai', () => {
+    test('should create Samurai class', () => {
+        let character = new Samurai('Tom Cruise');
+        expect(character.name).toEqual('Tom Cruise');
+        expect(character.intelligence).toEqual(10);
+        expect(character.strength).toEqual(10);
+        expect(character.magic).toEqual(0);
+        expect(character.level).toEqual(1);
         expect(character.hp).toEqual(100);
 
     });
+  });
+  describe('Vin Diesel', () => {
+    test('should create Vin Diesel obj', () => {
+        let character = new VinDiesel('Vin Diesel');
+        expect(character.name).toEqual('Vin Diesel');
+        expect(character.intelligence).toEqual(20);
+        expect(character.strength).toEqual(20);
+        expect(character.magic).toEqual(20);
+        expect(character.level).toEqual(20);
+        expect(character.hp).toEqual(200);
+
+    });
+  });
+  describe('Ninja', () => {
+    test('should create Ninja class', () => {
+        let character = new Ninja('Michelangelo');
+        expect(character.name).toEqual('Michelangelo');
+        expect(character.intelligence).toEqual(10);
+        expect(character.strength).toEqual(3);
+        expect(character.magic).toEqual(2);
+        expect(character.level).toEqual(1);
+        expect(character.hp).toEqual(90);
+
+    });
+  });
+
+  describe('Warrior', () => {
+    test('should create Warrior class', () => {
+        let character = new Warrior('Mulan');
+        expect(character.name).toEqual('Mulan');
+        expect(character.intelligence).toEqual(5);
+        expect(character.strength).toEqual(5);
+        expect(character.magic).toEqual(0);
+        expect(character.level).toEqual(1);
+        expect(character.hp).toEqual(90);
+
+    });
+  });
+
+  //levelUp
+  describe('Player', () => {
+        test('should level up characters', () => {
+        let character = new Warrior('Mulan');
+        character.levelUp();
+        expect(character.name).toEqual('Mulan');
+        expect(character.intelligence).toEqual(6);
+        expect(character.strength).toEqual(6);
+        expect(character.magic).toEqual(1);
+        expect(character.level).toEqual(2);
+        expect(character.hp).toEqual(91);
+  });
+
 
 });
--- src/rpg.js
+++ src/rpg.js
@@ -1,19 +1,60 @@
-export class Wizard {
+export class Player {
+    //create Player object
     constructor(name, intelligence, strength, magic, level, hp) {
-      this.name = name;
-      this.intelligence = 10;
-      this.strength = 2;
-      this.magic = 10;
-      this.level = 1;
-      this.hp = 100;
+        this.name = name;
+        this.intelligence = intelligence;
+        this.strength = strength;
+        this.magic = magic;
+        this.level = level;
+        this.hp = hp;
     }
 
-  }
+    //method to levelUp()
+    levelUp() {
+        this.intelligence +=1;
+        this.strength += 1;
+        this.magic += 1;
+        this.level += 1;
+        this.hp += 1;
+    }
+}
 
-  let levelUp = function() {
-    this.intelligence =+1;
-    this.strength =+1;
-    this.magic =+1;
-    this.level =+1;
-    this.hp =+1;
-  }
+//create Wizard object
+export class Wizard extends Player {
+    constructor(name, intelligence, strength, magic, level, hp) {
+        super(name,10,2,10,1,90);
+    }
+
+}
+
+//create Samurai object
+export class Samurai extends Player {
+    constructor(name, intelligence, strength, magic, level, hp) {
+        super(name,10,10,0,1,100);
+    }
+
+}
+
+//create Ninja object
+export class Ninja extends Player {
+    constructor(name, intelligence, strength, magic, level, hp) {
+        super(name,10,3,2,1,90);
+    }
+
+}
+
+//create VinDiesel object
+export class VinDiesel extends Player {
+    constructor(name, intelligence, strength, magic, level, hp) {
+        super(name,20,20,20,20,200);
+    }
+
+}
+
+//create Warrior object
+export class Warrior extends Player {
+    constructor(name, intelligence, strength, magic, level, hp) {
+        super(name,5,5,0,1,90);
+    }
+
+}
