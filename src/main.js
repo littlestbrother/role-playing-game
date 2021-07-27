ddec050d60817cce1648e7f46651f663fce442ba
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-07-26 20:06:40 -0700

    add if statement to navigate scenarios, and add scenario usability

--- src/main.js
+++ src/main.js
@@ -2,18 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import {charTypeSelect, confirmCharacter} from './scenarios.js'
+import { charTypeSelect, confirmCharacter } from './scenarios.js'
 
-const progression = [charTypeSelect,confirmCharacter];
+const progression = [charTypeSelect, confirmCharacter];
+export let name = null; 
 
 $(document).ready(function () {
-    //let int = 0
+    let int = 0
     // $('pre#result').text('Welcome to:\n\n ▄▄▄██▀▀▀▓█████   ██████ ▄▄▄█████▓ ▒█████   ██▓███   ▒█████   ██▓     ██▓  ██████ \n   ▒██   ▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██░  ██▒▒██▒  ██▒▓██▒    ▓██▒▒██    ▒ \n   ░██   ▒███   ░ ▓██▄   ▒ ▓██░ ▒░▒██░  ██▒▓██░ ██▓▒▒██░  ██▒▒██░    ▒██▒░ ▓██▄   \n▓██▄██▓  ▒▓█  ▄   ▒   ██▒░ ▓██▓ ░ ▒██   ██░▒██▄█▓▒ ▒▒██   ██░▒██░    ░██░  ▒   ██▒\n ▓███▒   ░▒████▒▒██████▒▒  ▒██▒ ░ ░ ████▓▒░▒██▒ ░  ░░ ████▓▒░░██████▒░██░▒██████▒▒\n ▒▓▒▒░   ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ▒░▒░▒░ ▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▓  ░░▓  ▒ ▒▓▒ ▒ ░\n ▒ ░▒░    ░ ░  ░░ ░▒  ░ ░    ░      ░ ▒ ▒░ ░▒ ░       ░ ▒ ▒░ ░ ░ ▒  ░ ▒ ░░ ░▒  ░ ░\n ░ ░ ░      ░   ░  ░  ░    ░      ░ ░ ░ ▒  ░░       ░ ░ ░ ▒    ░ ░    ▒ ░░  ░  ░  \n ░   ░      ░  ░      ░               ░ ░               ░ ░      ░  ░ ░        ░  \n\nPlease type your name:')
     $("form").submit(function (event) {
         event.preventDefault();
-        progression[1]();
-        
-        // }
+        name = $('input#interact').val();
+        //let name = $('input#interact').val();
+        // console.log(name);
+
+        if (int < 9) {
+            $("input#interact").val("");
+            // console.log(progression[int])
+            progression[int]();
+            int++
+        }
     });
 });
-
--- src/scenarios.js
+++ src/scenarios.js
@@ -1,13 +1,15 @@
 import $ from 'jquery';
-// export function Scenarios(){
+import {name} from './main.js'
 export function charTypeSelect() {
-    let interact = $('input#interact').val();
-    $('pre#result').text('Your character name is: ' + interact + '\n\nWhat character type would you like to be?\n\nNinja, Samurai, Warrior, or Wizard?');
+   $('input#interact').val('');
+    $('pre#result').text('Hello ' + name + ', what character type are you? ');
+    $('div#textbox').hide();
+    $('div#other').append('<button id="wizard" value="wizard" type="submit">* Wizard</button><br><button id="samurai" value="samurai" type="submit">* Samurai</button>')
+    // '\n\nWhat character type would you like to be?\n\nNinja, Samurai, Warrior, or Wizard?'
 }
 
 export function confirmCharacter() {
     $('div#textbox').hide();
-    $('div#console').append('<input type="radio" id="html" name="fav_language" value="HTML"><label for="html">HTML</label><br><input type="radio" id="css" name="fav_language" value="CSS"><label for="css">CSS</label><br><input type="radio" id="javascript" name="fav_language" value="JavaScript"><label for="javascript">JavaScript</label>');
+    $('pre#result').text('This is confirmChar');
+    // let name = $('input#interact').val();
 }
-
-// }
